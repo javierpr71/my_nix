eb0bca633740bce85f9096a8156143916cb2f8a8
javierpr71 <>
2022-10-28 17:11:30 +0200

    my overlay

---
+++ default.nix
@@ -0,0 +1,15 @@
+let
+  sources = imports ./nix/sources.nix;
+
+  hello = pkgs.writeShellScriptBin "hello" ''
+    echo "Hello from the Nix channel overlay!"
+  '';
+
+  pkgs = import sources.nixpkgs {
+    overlays = [
+      (self: super: {
+        inherit hello;
+      })
+    ];
+  };
+in pkgs
